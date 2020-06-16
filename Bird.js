353363484dcd09e9cc7ae67938609e22a923b304
vidur843 <65580238+vidur843@users.noreply.github.com>
2020-06-16 18:34:53 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,10 @@
+class Bird extends Base{
+    constructor(x, y) {
+      super(x,y,50,50);
+      this.image=loadImage("sprites/bird.png");
+     }
+    display()      {
+      
+      super.display();
+    }
+    }
